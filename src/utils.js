ada91c0a8d18a32e8b22644629ec957e5c7d8e73
Andrew Krupitsa <andrew.krupitsa@gmail.com>
2020-04-02 22:01:33 +0300

    added superSumm function

--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,9 @@ function multiply(a, b) {
   return a * b;
 }
 
-function sum(a, b) {
-    return a + b;
+export function sum(a, b) {
+  return a + b;
 }
+
+superSumm = (...numbers) =>
+  numbers.length !== 0 ? (numbers.every(n => typeof n === 'number') ? numbers.reduce((sum, number) => sum + number) : null) : null;
